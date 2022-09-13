a25bf4da8a9aed03b0c950ef840f9f822109ce4a
Morten Johansson <morten.johansson@outlook.com>
2022-09-13 17:14:46 +0200

    Added js comments

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// Script
+/* 
+  Things that need to happen
+  1. Get value from input
+  2. Convert Btn eventlistener
+  3. Convert value from input to number
+  4. Use number and convert to each unit req
+  5. Render out results to fields
+*/
