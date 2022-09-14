1a5d3239eceae03362268863e48ef99afa21a572
Morten Johansson <morten1.kodehode@gmail.com>
2022-09-14 12:57:41 +0200

    finished the logic

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-/* 
-  Things that need to happen
-  1. Get value from input
-  2. Convert Btn eventlistener
-  3. Convert value from input to number
-  4. Use number and convert to each unit req
-  5. Render out results to fields
-*/
+const convertBtn = document.getElementById("convertBtn");
+const inputBox = document.getElementById("inputBox");
+
+convertBtn.addEventListener("click", function () {
+  convert();
+});
+
+function convert() {
+  const value = inputBox.value;
+  const lengthOutput = document.getElementById("lengthOutput");
+  const volumeOutput = document.getElementById("volumeOutput");
+  const massOutput = document.getElementById("massOutput");
+
+  lengthOutput.textContent = `${value} meters = ${value * (0.3048).toFixed(3)} 
+    feet ||
+    ${value} feet = ${value * (3.281).toFixed(3)} meters`;
+
+  volumeOutput.textContent = `${value} litres = ${value * (0.264).toFixed(3)} 
+    gallons ||
+    ${value} gallons = ${value * (4.54609).toFixed(3)} litres`;
+
+  massOutput.textContent = `${value} kilos = ${value * (2.204).toFixed(3)}
+   pounds ||
+   ${value} pounds = ${value * (0.4535924).toFixed(3)} kilos`;
+}
